374cff6ca4850cb17ab39ef9df2e34ec5785ad0f
arct <brian@arct.io>
2021-02-06 11:22:46 -0800

    :recycle: Refactor DSL syntax

--- ftclib/src/main/kotlin/io/arct/ftc/hardware/input/GamepadDsl.kt
+++ ftclib/src/main/kotlin/io/arct/ftc/hardware/input/GamepadDsl.kt
@@ -1,29 +1,30 @@
 package io.arct.ftc.hardware.input
 
 import io.arct.rl.hardware.input.Button
+import io.arct.rl.hardware.input.Controller
 import io.arct.rl.hardware.input.DPad
 import io.arct.rl.hardware.input.Joystick
 
 class GamepadDsl internal constructor() {
-    internal val click: MutableList<Triple<Gamepad.() -> Button, Boolean, () -> Unit>> = mutableListOf()
-    internal val active: MutableList<Pair<Gamepad.() -> Boolean, () -> Unit>> = mutableListOf()
+    internal val click: MutableList<Triple<(Controller) -> Button, Boolean, () -> Unit>> = mutableListOf()
+    internal val active: MutableList<Pair<(Controller) -> Boolean, () -> Unit>> = mutableListOf()
 
-    fun click(condition: Gamepad.() -> Button, rising: Boolean = true, action: () -> Unit) =
+    fun click(condition: (Controller) -> Button, rising: Boolean = true, action: () -> Unit) =
         click.add(Triple(condition, rising, action))
 
     @JvmName("clickJoystick")
-    fun click(joystick: Gamepad.() -> Joystick, rising: Boolean = true, action: () -> Unit) =
-        click(fun(gamepad: Gamepad): Button = joystick(gamepad), rising, action)
+    fun click(joystick: (Controller) -> Joystick, rising: Boolean = true, action: () -> Unit) =
+        click(fun(gamepad: Controller): Button = joystick(gamepad), rising, action)
 
     @JvmName("clickDPad")
     fun click(button: (DPad) -> Button, rising: Boolean = true, action: () -> Unit) =
-        click(fun(gamepad: Gamepad): Button = button(gamepad.dpad), rising, action)
+        click(fun(gamepad: Controller): Button = button(gamepad.dpad), rising, action)
 
-    fun active(condition: Gamepad.() -> Boolean, action: () -> Unit) =
+    fun active(condition: (Controller) -> Boolean, action: () -> Unit) =
         active.add(condition to action)
 
-    fun active(trigger: Gamepad.() -> Double, threshold: Double = 0.5, action: () -> Unit) =
-        active(fun(gamepad: Gamepad): Boolean = if (threshold >= 0) trigger(gamepad) > 0.5 else trigger(gamepad) < -threshold, action)
+    fun active(trigger: (Controller) -> Double, threshold: Double = 0.5, action: () -> Unit) =
+        active(fun(gamepad: Controller): Boolean = if (threshold >= 0) trigger(gamepad) > 0.5 else trigger(gamepad) < -threshold, action)
 
     fun active(action: () -> Unit) =
         active({ true }, action)
